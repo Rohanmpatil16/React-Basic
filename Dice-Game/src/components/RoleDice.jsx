b3278c77bef2aa4f330d27b3b897fd48bc433d5b
Rohanmpatil16 <rohanmpatil18@gmail.com>
2026-04-03 23:18:36 +0530

    work done

--- Dice-Game/src/components/RoleDice.jsx
+++ Dice-Game/src/components/RoleDice.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import styled from "styled-components"
 
 const RoleDice = () => {
   return (
-    <div>RoleDice</div>
+    <DiceContainer>
+    <div><img src="/images/dice_2.png" alt="Dice 2" /></div>
+    <p>Click on Dice to roll</p>
+    </DiceContainer>
   )
 }
 
 export default RoleDice
+
+const DiceContainer=styled.div`
+margin-top:40px;
+display:flex;
+flex-directions:column;
+
+`
