ed8388b6edb6316a838c97ba6de6d0c87d512af7
Rohanmpatil16 <rohanmpatil18@gmail.com>
2026-08-15 22:51:21 +0530

    Added a Increment, decrement, reset using usestate

--- revision--/src/App.jsx
+++ revision--/src/App.jsx
@@ -3,18 +3,18 @@ import { useState } from "react";
 function App()
 {
   
-  const [a,setA] = useState("rohan");
+  const [a,setA] = useState(1);
 
-  let change=()=>{
-    setA("rohit");
-  }
+ 
   return (
 
     <div>
       
-      <h1>Hello, World! {a}</h1>
+      <h1>Count: {a}</h1>
      
-      <button onClick={change}>click me</button>
+      <button onClick={()=>setA(a+1)}>Increment</button>
+      <button onClick={()=>setA(a-1)}>Decrement</button>
+      <button onClick={()=>setA(0)}>Reset</button>
       
     </div>
   );
