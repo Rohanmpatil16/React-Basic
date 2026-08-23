962be6d40cab12da8bd75ab5be8b635f23d79c0a
Rohanmpatil16 <rohanmpatil18@gmail.com>
2026-08-23 21:03:20 +0530

    Work on error

--- revision--/src/App.jsx
+++ revision--/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import About from './components/About'
 import Home from './components/Home'
+import { Route,Routes } from 'react-router-dom'
 const App = () => {
   return (
     <div>
+      <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/about' element={<About/>} />
+      </Routes>
     </div>
   )
 }
