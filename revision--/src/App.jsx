08f104f90181b3ea2ac4b14e84ae5cd070c86f39
Rohanmpatil16 <rohanmpatil18@gmail.com>
2026-08-27 22:58:37 +0530

    added more pages

--- revision--/src/App.jsx
+++ revision--/src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
+import Contact from "./components/Contact";
+import Profile from "./components/Profile";
+import NotFound from "./components/NotFound";
 
 const App = () => {
   return (
@@ -11,8 +15,22 @@ const App = () => {
       <Navbar />
 
       <Routes>
+
+        {/* Home Page */}
         <Route path="/" element={<Home />} />
+
+        {/* About Page */}
         <Route path="/about" element={<About />} />
+
+        {/* Contact Page */}
+        <Route path="/contact" element={<Contact />} />
+
+        {/* Profile Page */}
+        <Route path="/profile/:name" element={<Profile />} />
+
+        {/* 404 Page */}
+        <Route path="*" element={<NotFound />} />
+
       </Routes>
 
     </div>
